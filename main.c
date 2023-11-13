3cc1b192706f3bfc87fc3352cd185e9fa93e085c
IvanMella <ivan.m48@gmail.com>
2023-11-14 00:30:59 +0100

    modifiche varie per compilare la prima versione parziale

--- main.c
+++ main.c
@@ -1,38 +1,39 @@
 # include <stdio.h>
 # include <stdlib.h>
+# include <stdint.h>
 
 typedef struct DatabaseRecords
 {
 	char fieldName[11];		// 0 - 10
-	char fieldType;			// 11
-	char reservedf[4];		// 12 - 15
-	unsigned char fieldLength;	// 16
-	char fieldDecimal;		// 17
-	char waid;			// 18 - 19
-	char example;			// 20
-	char reserveds[10];		// 21 - 30
-	char mdxflag;			// 31
+	uint8_t fieldType;		// 11
+	uint8_t reservedf[4];		// 12 - 15
+	uint8_t fieldLength;		// 16
+	uint8_t fieldDecimal;		// 17
+	uint8_t waid;			// 18 - 19
+	uint8_t example;		// 20
+	uint8_t reserveds[10];		// 21 - 30
+	uint8_t mdxflag;		// 31
 } dbr;
 
 typedef struct FieldDescriptorArray
 {
-	char vdbase;		// 0
-	char open[3];		// 1 - 3
-	long recordsdb;		// 4 - 7
-	int headerb;		// 8 - 9
-	int recordb;		// 10 - 11
-	char reservedf[2];	// 12 - 13
-	char transaction;	// 14
-	char encryptionflag;	// 15
-	char reserveds[12];	// 16 - 27
-	char mdxflag;		// 28
-	char driverid;		// 29
-	char reservedt[2];	// 30 - 31
-	dbr *fd;		// 32 - n
-	char end;		// n + 1
+	uint8_t vdbase;			// 0
+	uint8_t open[3];		// 1 - 3
+	uint32_t recordsdb;		// 4 - 7
+	uint16_t headerb;		// 8 - 9
+	uint16_t recordb;		// 10 - 11
+	uint8_t reservedf[2];		// 12 - 13
+	uint8_t transaction;		// 14
+	uint8_t encryptionflag;		// 15
+	uint8_t reserveds[12];		// 16 - 27
+	uint8_t mdxflag;		// 28
+	uint8_t driverid;		// 29
+	uint8_t reservedt[2];		// 30 - 31
+	dbr *fd;			// 32 - n
+	uint8_t end;			// n + 1
 } fda;
 
-void printchars(char *, unsigned char *, int);
+void printchars(char *, uint8_t *, int);
 void print_header(fda *);
 
 
@@ -73,11 +74,9 @@ void print_header(fda *datas)
 	printchars("reservedt:\t", datas->reservedt, sizeof(datas->reservedt));
 
 	printf("end:\t%x\n", datas->end);
-
-
 }
 
-void printchars(char * text, unsigned char *c, int size)
+void printchars(char * text, uint8_t *c, int size)
 {
 	printf("%s", text);
 	for(int i = 0; i < size - 1; i++)
